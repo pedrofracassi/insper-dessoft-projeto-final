b204f3b5de463cb14b54118b0190016c3f8f38bc
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-05-23 00:50:21 -0300

    criando o player

--- stages/jogo.py
+++ stages/jogo.py
@@ -6,3 +6,23 @@ class Stage(BaseStage):
 
   def run (self):
     self.window.fill((0, 10, 0))
+
+#Definindo a Alessandra
+alessandra = pygame.image.load('referencia/png-teste.png').convert()
+alessandra_x = 2
+alessandra_y = 0
+
+#Definindo as posições iniciais
+alessandra_x = 2
+alessandra_y = 0
+
+#Definindo a velocidade por eixo(isso depois precisa mudar de acordo com os obstaculos e boosts)
+alessandra_speedx = -0.5
+alessandra_speedy = 0
+
+#Atualizando a posição (esse dentro do for events)
+alessandra_x+=alessandra_speedx
+alessandra_y+=alessandra_speedy
+
+#saída
+window.blit(alessandra, (alessandra_x, alessandra_y))
