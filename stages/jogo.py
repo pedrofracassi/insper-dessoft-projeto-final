5821e324b51b79c4293c119cdbc4c109517fe064
Pedro Fracassi <pedroluizfracassi@gmail.com>
2021-05-23 00:55:09 -0300

    fixes

--- stages/jogo.py
+++ stages/jogo.py
@@ -1,3 +1,4 @@
+import pygame
 from stage import BaseStage
 
 class Stage(BaseStage):
@@ -7,22 +8,22 @@ class Stage(BaseStage):
   def run (self):
     self.window.fill((0, 10, 0))
 
-#Definindo a Alessandra
-alessandra = pygame.image.load('referencia/png-teste.png').convert()
-alessandra_x = 2
-alessandra_y = 0
+    #Definindo a Alessandra
+    alessandra = pygame.image.load('referencia/png-teste.png').convert()
+    alessandra_x = 2
+    alessandra_y = 0
 
-#Definindo as posições iniciais
-alessandra_x = 2
-alessandra_y = 0
+    #Definindo as posições iniciais
+    alessandra_x = 2
+    alessandra_y = 0
 
-#Definindo a velocidade por eixo(isso depois precisa mudar de acordo com os obstaculos e boosts)
-alessandra_speedx = -0.5
-alessandra_speedy = 0
+    #Definindo a velocidade por eixo(isso depois precisa mudar de acordo com os obstaculos e boosts)
+    alessandra_speedx = -0.5
+    alessandra_speedy = 0
 
-#Atualizando a posição (esse dentro do for events)
-alessandra_x+=alessandra_speedx
-alessandra_y+=alessandra_speedy
+    #Atualizando a posição (esse dentro do for events)
+    alessandra_x+=alessandra_speedx
+    alessandra_y+=alessandra_speedy
 
-#saída
-window.blit(alessandra, (alessandra_x, alessandra_y))
+    #saída
+    self.window.blit(alessandra, (alessandra_x, alessandra_y))
--- stages/menu.py
+++ stages/menu.py
@@ -4,13 +4,32 @@ from stage import BaseStage
 class Stage(BaseStage):
   def __init__(self, window, state) -> None:
       super(Stage, self).__init__('menu', window, state)
+      self.y = 0
+      self.x = 0
 
   def run (self):
     game = True
     while game:
+      pygame.time.Clock().tick(60)
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           game = False
-      self.window.fill((255, 0, 0))
+
+      keys = pygame.key.get_pressed()
+
+      if keys[pygame.K_UP]:
+        self.y -= 1
+      if keys[pygame.K_DOWN]:
+        self.y += 1
+      if keys[pygame.K_LEFT]:
+        self.x -= 1
+      if keys[pygame.K_RIGHT]:
+        self.x += 1
+
+      self.window.fill((255, 255, 255))
+
+      cor = (255, 0, 0)
+      vertices = [(self.x, self.y), (self.x, self.y + 10), (self.x + 10, self.y), (self.x + 10, self.y + 10)]
+      pygame.draw.polygon(self.window, cor, vertices)
+
       pygame.display.update()
-      
