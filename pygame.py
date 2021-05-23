1683a71f39e4dc3a7bd95624c5ede78631a24c3f
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-05-22 22:24:26 -0300

    ,

---
+++ pygame.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+#Inicializando a janela/nome da janela
+janela_principal = pygame.display.set_mode((600, 500))
+titulo = pygame.display.set_caption('Corrida contra a DP')
+#Laço principal
+game = True
