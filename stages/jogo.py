0727d7a33757a9111aadcf4f0f2599905b885b79
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-05-23 15:17:33 -0300

    implementando o obstaculo e o pulo

--- stages/jogo.py
+++ stages/jogo.py
@@ -10,45 +10,74 @@ class Stage(BaseStage):
 
 
 pygame.init()
-window = pygame.display.set_mode((480,600))
+window = pygame.display.set_mode((600,500))
 #Definindo a Alessandra
 x = 48
 y = 60
 alessandr = pygame.image.load('png-teste.png').convert()
 alessandra = pygame.transform.scale(alessandr, (x, y))
-
+#Definindo 
 game = True
 #Definindo a posição inicial
-alessandra_x = 24
-alessandra_y = 120
-
-#Definindo a velocidade por eixo(isso depois precisa mudar de acordo com os obstaculos e boosts)
-alessandra_speedx = 0.5
-alessandra_speedy = 0
+alessandra_x = 300
+alessandra_y = 350
+#Definindo obstáculo
+c = pygame.image.load('cone.png').convert()
+cone = pygame.transform.scale(c, (30, 40))
+cone_x = 0
+cone_y = 370
+#Definindo score
+i=0
+score = 0
+#Definindo as vidas
+lives = 3
 
 while game:
   pygame.time.Clock().tick(60)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       game = False
-
-  keys = pygame.key.get_pressed()
-
-  if alessandra_y>0:
-    alessandra_y = 0
-  if keys[pygame.K_UP]:
-    alessandra_y += 2
   
-  if keys[pygame.K_LEFT]:
-    alessandra_x += alessandra_speedx
-  if keys[pygame.K_RIGHT]:
-    alessandra_x -= alessandra_speedx 
+  #SCORE
+  font = pygame.font.SysFont(None, 20)
+  texto = font.render('Score: {}'.format(score), True, (0, 0, 255))
+  #Atualizando score
+  i+=1
+  if i==50:
+    score+=10
+    i=0
+  #VIDAS
+  font = pygame.font.SysFont(None, 20)
+  texto2 = font.render('Vidas: {}'.format(lives), True, (0, 255, 0))
+  #Atualizando as vidas
+  '''colisão = 
+  if len(colisão)>0:
+    lives-=1'''
+  if lives == 0:
+    game = False
+
+  #Atualizando posição do cone
+  cone_x+=1
+  if cone_x == 599:
+    cone_x = 0
+  #Atualizando posição Alesssandra
+  keys = pygame.key.get_pressed()
+  if alessandra_y<350:
+    alessandra_y = 350
+  if keys[pygame.K_UP]:
+        alessandra_y -=50
+  
+
+  
   
 
  
 
-  window.fill((0, 0, 0))
+  window.fill((0,0,0))
   window.blit(alessandra, (alessandra_x, alessandra_y))
+  window.blit(cone,(cone_x,cone_y))
+  window.blit(texto, (500, 10))
+  window.blit(texto2, (500, 30))
   pygame.display.update()
 
   
