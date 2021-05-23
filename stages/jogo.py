dd3d7b21a3a5c1bc3d29054e731152ddb88eead2
Camilabernardimoniz <camilabm3@al.insper.edu.br>
2021-05-23 01:08:59 -0300

    alessandra movimentos

--- stages/jogo.py
+++ stages/jogo.py
@@ -9,21 +9,27 @@ class Stage(BaseStage):
     self.window.fill((0, 10, 0))
 
     #Definindo a Alessandra
-    alessandra = pygame.image.load('referencia/png-teste.png').convert()
+    x = 48
+    y = 60
+    alessandr = pygame.image.load('referencia/png-teste.png').convert()
+    alessandra = pygame.transform.scale(alessandr, (x, y))
     alessandra_x = 2
     alessandra_y = 0
 
     #Definindo as posições iniciais
-    alessandra_x = 2
-    alessandra_y = 0
+    alessandra_x = 360
+    alessandra_y = 400
 
     #Definindo a velocidade por eixo(isso depois precisa mudar de acordo com os obstaculos e boosts)
-    alessandra_speedx = -0.5
+    alessandra_speedx = -0.1
     alessandra_speedy = 0
 
     #Atualizando a posição (esse dentro do for events)
     alessandra_x+=alessandra_speedx
     alessandra_y+=alessandra_speedy
+    #Se ela atravessar a tela ela volta pro inicio dela
+    if alessandra_x<-2:
+        alessandra_x = 360
 
     #saída
     self.window.blit(alessandra, (alessandra_x, alessandra_y))
