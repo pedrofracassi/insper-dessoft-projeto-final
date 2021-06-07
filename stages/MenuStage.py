c2cf18a6da9ddaf50d7d04b93611623947adfdfe
Pedro Fracassi <pedroluizfracassi@gmail.com>
2021-06-07 00:56:52 -0300

    fix menu

--- stages/MenuStage.py
+++ stages/MenuStage.py
@@ -13,6 +13,14 @@ class Stage(structures.BaseStage.BaseStage):
   def run (self):
     button_start = button.button(100, 100, 300, 100, 'BOTÃO DAORA')
 
+    font = pygame.font.SysFont(None, 32)
+    texto = font.render('APERTE ESPAÇO PARA INICIAR', True, (0, 0, 255))
+    texto2 = font.render('MENU DAORA', True, (255, 255, 255))
+    largura, altura = font.size('APERTE ESPAÇO PARA INICIAR')
+    largura2, altura2 = font.size('MENU DAORA')
+    self.window.blit(texto, (WIDTH / 2 - largura / 2, HEIGHT / 2 - altura/2))
+    self.window.blit(texto2, (WIDTH / 2 - largura2 / 2, HEIGHT / 2 - altura/2 - 20))
+
     done = False
     while not done:
       pygame.time.Clock().tick(60)
@@ -20,17 +28,9 @@ class Stage(structures.BaseStage.BaseStage):
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           done = True
+          return
         if event.type == pygame.KEYDOWN:
           done = True
           return 'game'
 
-      font = pygame.font.SysFont(None, 32)
-
-      texto = font.render('APERTE ESPAÇO PARA INICIAR', True, (0, 0, 255))
-      texto2 = font.render('MENU DAORA', True, (255, 255, 255))
-      largura, altura = font.size('APERTE ESPAÇO PARA INICIAR')
-      largura2, altura2 = font.size('MENU DAORA')
-      self.window.blit(texto, (WIDTH / 2 - largura / 2, HEIGHT / 2 - altura/2))
-      self.window.blit(texto2, (WIDTH / 2 - largura2 / 2, HEIGHT / 2 - altura/2 - 20))
       pygame.display.update()
-      pygame.display.flip()
