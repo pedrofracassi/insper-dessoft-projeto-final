9606b68f1cbf92902b24171bdf621a9c98f1224d
Pedro Fracassi <pedroluizfracassi@gmail.com>
2021-06-07 02:01:56 -0300

    menu bonito

--- sprites/button.py
+++ sprites/button.py
@@ -10,14 +10,21 @@ class button (pygame.sprite.Sprite):
     self.height = height
     self.text = text
 
-  def draw (self, window):
-    color = (255, 0, 0) if self.isBeingHovered() else (0, 255, 0)
-    pygame.draw.rect(window, color, (self.x, self.y, self.width, self.height), 0)
+    self.hover_move = 10
 
+  def draw (self, window):
     font = pygame.font.SysFont(None, 32)
     text = font.render(self.text, 1, (0, 0, 0))
-    window.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    if not self.isBeingHovered():
+      pygame.draw.rect(window, (0, 234, 249), (self.x, self.y, self.width, self.height), 0)
+      pygame.draw.rect(window, (0, 192, 204), (self.x, self.y + self.height - self.hover_move, self.width, self.hover_move), 0)
+      window.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + ((self.height - self.hover_move)/2 - text.get_height()/2)))
+    else:
+      pygame.draw.rect(window, (0, 234, 249), (self.x, self.y + self.hover_move, self.width, self.height - self.hover_move), 0)
+      window.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + ((self.height - self.hover_move)/2 - text.get_height()/2 + self.hover_move)))
+
 
   def isBeingHovered (self):
     mouse_pos = pygame.mouse.get_pos()
-    return mouse_pos[0] > self.x and mouse_pos[0] < self.x + self.width and mouse_pos[1] > self.y and mouse_pos[1] < self.x + self.height
+    return mouse_pos[0] > self.x and mouse_pos[0] < self.x + self.width and mouse_pos[1] > self.y and mouse_pos[1] < self.y + self.height
--- stages/MenuStage.py
+++ stages/MenuStage.py
@@ -13,16 +13,11 @@ class Stage(structures.BaseStage.BaseStage):
 
   def run (self):
     bg = MenuBG.MenuBG()
-    button_start = button.button(WIDTH - 300, HEIGHT - 150, 250, 100, 'Jogar')
+    btn_width = 200
+    btn_height = 70
+    button_start = button.button(WIDTH - btn_width - 50, 50, btn_width, btn_height, 'Jogar')
     sprites = pygame.sprite.Group()
     sprites.add(bg)
-    font = pygame.font.SysFont(None, 32)
-    texto = font.render('APERTE ESPAÇO PARA INICIAR', True, (0, 0, 255))
-    texto2 = font.render('MENU DAORA', True, (255, 255, 255))
-    largura, altura = font.size('APERTE ESPAÇO PARA INICIAR')
-    largura2, altura2 = font.size('MENU DAORA')
-    self.window.blit(texto, (WIDTH / 2 - largura / 2, HEIGHT / 2 - altura/2))
-    self.window.blit(texto2, (WIDTH / 2 - largura2 / 2, HEIGHT / 2 - altura/2 - 20))
 
     done = False
     while not done:
